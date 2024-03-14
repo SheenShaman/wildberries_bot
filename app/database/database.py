67d4fe9070b51146861e9e1f8e5e5a879d4e92b1
Danil Sheen <dan.80956@gmail.com>
2024-03-14 14:31:36 +0300

    added handlers get_info_database and stop_notification

--- app/database/database.py
+++ app/database/database.py
@@ -8,7 +8,6 @@ from app.config import DATABASE_URL
 load_dotenv()
 
 Base = declarative_base()
-
 engine = create_engine(DATABASE_URL)
 Base.metadata.create_all(bind=engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
