ad42af1de6d695ef67fbff54cbc463d0f1235127
Danil Sheen <dan.80956@gmail.com>
2024-03-14 14:31:45 +0300

    added handlers get_info_database and stop_notification

--- app/handlers.py
+++ app/handlers.py
@@ -9,6 +9,8 @@ from app.database.models import History
 from app.keyboards import main, inline_keyboard
 from app.product_info import get_product_info
 
+session = SessionLocal()
+
 router = Router()
 
 
@@ -46,17 +48,55 @@ async def get_product(message: Message, state: FSMContext):
         )
         await state.set_state(Article.numbers)
     else:
-        with SessionLocal() as session:
-            session.add(
-                History(
-                    user_id=message.from_user.id,
-                    article=article
-                )
+        session.add(
+            History(
+                user_id=message.from_user.id,
+                article=article
             )
-            session.commit()
+        )
+        session.commit()
 
         product_info = ",\n".join([f'{key} - {value}' for key, value in product_response.items()])
         await message.answer(
             text=product_info,
+            reply_markup=inline_keyboard().as_markup()
+        )
+
+
+@router.message(F.text == "Получить информацию из БД")
+async def get_info_database(message: Message, state: FSMContext):
+    last_info = session.query(History).all()[-5:]
+    if last_info:
+        for query in last_info:
+            query_time = query.query_time.strftime("%d.%m.%Y %H:%M")
+            await message.answer(
+                text=(
+                    f"id Пользователя - {query.user_id}\n"
+                    f"Время запроса - {query_time}\n"
+                    f"Артикул товара - {query.article}\n"
+                ),
+                reply_markup=inline_keyboard().as_markup()
+            )
+    else:
+        await message.answer(
+            text="Отсуствуют данные.",
+            reply_markup=inline_keyboard().as_markup()
+        )
+
+
+@router.message(F.text == "Остановить уведомления")
+async def stop_notification(message: Message, state: FSMContext):
+    user_id = message.from_user.id
+    user_history = session.query(History).filter(History.user_id == user_id).order_by(History.query_time.desc()).first
+    if user_history and user_history.subscribe is True:
+        user_history.subscribe = False
+        session.commit()
+        await message.answer(
+            text="Вы отписаны от уведомлений.",
+            reply_markup=inline_keyboard().as_markup()
+        )
+    else:
+        await message.message.answer(
+            text="Вы не подписаны на уведомления.",
             reply_markup=inline_keyboard().as_markup()
         )
